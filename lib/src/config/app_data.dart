557702c3bfeb3b03ceadbaa92e3226c84de326b7
carlos731 <chtsca2001@gmail.com>
2023-05-10 15:57:51 -0300

    Adicionando projeto quase completo ao repositório

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,101 @@
+import '../models/cart_item_model.dart';
+import '../models/item_model.dart';
+import '../models/user_model.dart';
+
+ItemModel apple = ItemModel(
+  description:
+      'A melhor maçã da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+  imgUrl: 'assets/fruits/apple.png',
+  itemName: 'Maçã',
+  price: 5.5,
+  unit: 'kg',
+);
+
+ItemModel grape = ItemModel(
+  imgUrl: 'assets/fruits/grape.png',
+  itemName: 'Uva',
+  price: 7.4,
+  unit: 'kg',
+  description:
+      'A melhor uva da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel guava = ItemModel(
+  imgUrl: 'assets/fruits/guava.png',
+  itemName: 'Goiaba',
+  price: 11.5,
+  unit: 'kg',
+  description:
+      'A melhor goiaba da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel kiwi = ItemModel(
+  imgUrl: 'assets/fruits/kiwi.png',
+  itemName: 'Kiwi',
+  price: 2.5,
+  unit: 'un',
+  description:
+      'O melhor kiwi da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel mango = ItemModel(
+  imgUrl: 'assets/fruits/mango.png',
+  itemName: 'Manga',
+  price: 2.5,
+  unit: 'un',
+  description:
+      'A melhor manga da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+ItemModel papaya = ItemModel(
+  imgUrl: 'assets/fruits/papaya.png',
+  itemName: 'Mamão papaya',
+  price: 8,
+  unit: 'kg',
+  description:
+      'O melhor mamão da região e que conta com o melhor preço de qualquer quitanda. Este item conta com vitaminas essenciais para o fortalecimento corporal, resultando em uma vida saudável.',
+);
+
+// Lista de produtos
+List<ItemModel> items = [
+  apple,
+  grape,
+  guava,
+  kiwi,
+  mango,
+  papaya,
+];
+
+List<String> categories = [
+  'Frutas',
+  'Grãos',
+  'Verduras',
+  'Temperos',
+  'Cereais',
+  'Carnes',
+  'Peixes',
+];
+
+//Lista de carrinhos
+List<CartItemModel> cartItems = [
+  CartItemModel(
+    item: apple,
+    quantity: 1,
+  ),
+  CartItemModel(
+    item: mango,
+    quantity: 1,
+  ),
+  CartItemModel(
+    item: guava,
+    quantity: 3,
+  ),
+];
+
+UserModel user = UserModel(
+  phone: '99 9 9999-9999',
+  cpf: '06369408700',
+  email: 'chtsca2001@gmail.com',
+  name: 'Carlos',
+  password: '1234',
+);
